e8e48483574aba3b127df1634fe74433d0698695
Puchkkoo <anupam16singh@gmail.com>
2025-03-27 23:03:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Basic route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running: http://localhost:${PORT}`);
+});
